f8d3069bf689c3e7693ae70d1f2879b8d405f109
ashishverma2831 <ashishverma510207@gmail.com>
2023-12-31 20:21:10 +0530

    initial setup done frontend + backend both

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-800 text-white text-xl'>
+      App hello
+      hjkakjnvsnkl
+    </div>
+  )
+}
+
+export default App
