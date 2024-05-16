8172f9c5de35bc678f570f734ff27063a8895130
ashishverma2831 <ashishverma510207@gmail.com>
2024-05-16 16:44:53 +0530

    chore: Update dependencies and add carousel component to homepage

--- frontend/src/components/SliderOne.jsx
+++ frontend/src/components/SliderOne.jsx
@@ -1,44 +1,147 @@
 import React from 'react'
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Link } from 'react-router-dom';
+import '../App.css'
 
 const SliderOne = () => {
 
+  // const responsive = {
+  //   superLargeDesktop: {
+  //     // the naming can be any, depends on you.
+  //     breakpoint: { max: 4000, min: 3000 },
+  //     items: 5
+  //   },
+  //   desktop: {
+  //     breakpoint: { max: 3000, min: 1024 },
+  //     items: 3
+  //   },
+  //   tablet: {
+  //     breakpoint: { max: 1024, min: 464 },
+  //     items: 2
+  //   },
+  //   mobile: {
+  //     breakpoint: { max: 464, min: 0 },
+  //     items: 1
+  //   }
+  // };
+
+  const data = [
+    {
+      title: 'Listings',
+      description: 'A free app that allows you to manage your Zomato listing directly from your smartphone',
+      image: 'https://b.zmtcdn.com/merchant-onboarding/be4f70ca22a3b31a84b3cf8ed811f0281600769259.png'
+    },
+    {
+      title: 'Online Ordering',
+      description: 'Start taking orders online from millions of users near you and deliver with Zomato...',
+      image: 'https://b.zmtcdn.com/merchant-onboarding/2c7dd621a502cde6f5051fc6d411b8881600769171.png'
+    },
+    {
+      title: 'Advertise',
+      description: 'For every marketing dollar spent, Zomato returns over 8x the investment...',
+      image: 'https://www.zomato.com/partner-with-us/static/media/ZomatoAdvertise.31195a83.svg'
+    },
+    {
+      title: 'Events',
+      description: 'Partner with us for India’s grandest food & entertainment carnival - “Zomaland”...',
+      image: 'https://www.zomato.com/partner-with-us/static/media/ZomatoEvent.c3e1f3a8.svg'
+    },
+    {
+      title: 'Hyperpure',
+      description: 'Supplies fresh and high quality ingredients to restaurant for serving delicious meals...',
+      image: 'https://www.zomato.com/partner-with-us/static/media/Hyperpure.25f37214.svg'
+    }
+  ]
+
   return (
     <>
-        <div className='bg-white'>
-            <div className='max-w-screen-lg mx-auto'>
-                <p>Our products</p>
-                <Swiper
-                    spaceBetween={50}
-                    slidesPerView={3}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    cssMode={true}
-                    navigation={true}
-                    pagination={true}
-                    mousewheel={true}
-                    keyboard={true}
-                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-    >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-                </Swiper>
-            </div>
+      <div className=' my-5 p-5'>
+        <div className='max-w-screen-lg mx-auto flex flex-col'>
+          <p className='text-4xl text-center my-5 font-normal'>Our Products</p>
+          <Carousel
+            className=''
+            // additionalTransfrom={0}
+            // arrows={false}
+            autoPlay={true}
+            autoPlaySpeed={2000}
+            // centerMode={false}
+            containerClass="container-with-dots"
+            dotListClass="react-multi-carousel-dot-list"
+            draggable={true}
+            // focusOnSelect={false}
+            infinite={true}
+            // itemClass=""
+            // keyBoardControl
+            // minimumTouchDrag={80}
+            // pauseOnHover
+            // renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={true}
+            // renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: {
+                  max: 3000,
+                  min: 1024
+                },
+                items: 3,
+                partialVisibilityGutter: 40
+              },
+              tablet: {
+                breakpoint: {
+                  max: 1024,
+                  min: 464
+                },
+                items: 2,
+                partialVisibilityGutter: 30
+              },
+              mobile: {
+                breakpoint: {
+                  max: 464,
+                  min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
+              }
+            }}
+            rewind={true}
+            rewindWithAnimation={true}
+            // rtl={false}
+            // shouldResetAutoplay={true}
+            showDots={true}
+            // renderDotsOutside={true}
+            // sliderClass=""
+            slidesToSlide={1}
+            swipeable={true}
+          >
+            {/* <div className='w-[300px] bg-gray-50 my-5 shadow-lg rounded-lg'>
+              <img className='bg-contain w-full' src={'https://www.zomato.com/partner-with-us/static/media/ZomatoEvent.c3e1f3a8.svg'} />
+              <div className=' flex flex-col gap-2 p-4'>
+                <p className='text-2xl font-semibold'>Events</p>
+                <p className='text-lg font-normal text-gray-700'>Partner with us for India’s grandest food & entertainment carnival - “Zomaland”...</p>
+                <Link className='text-xl text-blue-500 font-normal'>Learn More </Link>
+              </div>
+            </div> */}
+
+            {
+              data.map((card) => {
+                return (
+                  <div className='w-[300px] bg-gray-50 my-5 shadow-lg rounded-lg mx-auto'>
+                    <img className='bg-contain w-full' src={card.image} />
+                    <div className=' flex flex-col gap-2 p-4'>
+                      <p className='text-2xl font-semibold'>{card.title}</p>
+                      <p className='text-lg font-normal text-gray-700'> {card.description} </p>
+                      <Link className='text-xl text-blue-500 font-normal'>Learn More </Link>
+                    </div>
+                  </div>
+                );
+              })
+            }
+
+
+          </Carousel>
         </div>
+      </div>
     </>
   )
 }
