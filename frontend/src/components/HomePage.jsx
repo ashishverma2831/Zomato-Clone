38029b553a38ae478c98ea254d1156f4299306b5
ashishverma2831 <ashishverma510207@gmail.com>
2024-05-23 15:17:17 +0530

    chore: Update HomePage banner section with improved styling and image layout

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
             <div className='bg-red-500 w-[320px]'>
                 <img className='w-full h-[150px]' src='https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*'/>
                 <div className='bg-blue-400 p-5'>
-                    <p className='text-xl'>Order Online</p>
+                    <p className='text-xl mb-1'>Order Online</p>
                     <p className='text-lg text-gray-500 font-thin'>Stay home and to your doorstep</p>
                 </div>
             </div>
