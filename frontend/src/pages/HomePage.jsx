7fbd7e71ca6596fb451917907e6c534336b09671
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-17 17:21:20 +0530

    Update HomePage component to display collections and improve styling

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -54,9 +54,37 @@ const HomePage = () => {
           <HomeCard cardData={cardData} />
         </Section>
 
-      <section className='py-10 text-center'>
-        <p className='text-4xl text-left px-5'>Explore options near me</p>
-        <div className='max-w-screen-lg text-center mx-auto my-10'>
+      <Section className='max-w-screen-xl mx-auto p-10'>
+        <div className='w-full'>
+          <p className='text-4xl text-left px-3'>Collections</p>
+          <div className='flex justify-between p-3'>
+            <p className='text-xl text-gray-500'>Explore curated lists of top restaurants, cafes, pubs, and bars in Lucknow, based on trends</p>
+            <p className='flex justify-between'><button className='text-red-500 text-xl'>All collections in Lucknow <i className="text-sm mx-2 fa-solid fa-play"></i></button></p>
+          </div>
+          <div className='p-3 flex gap-4 justify-evenly flex-wrap'>
+            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+              <p>Best insta-worthy places</p>
+              <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
+            </div>
+            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+              <p>Best insta-worthy places</p>
+              <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
+            </div>
+            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+              <p>Best insta-worthy places</p>
+              <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
+            </div>
+            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+              <p>Best insta-worthy places</p>
+              <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
+            </div>
+          </div>
+        </div>
+        </Section>
+
+      <section className='py-10 text-center '>
+        <p className='text-4xl mx-auto max-w-screen-lg text-left px-5'>Explore options near me</p>
+        <div className='max-w-screen-lg  text-center mx-auto my-10'>
 
           <div
             id="accordion-flush"
