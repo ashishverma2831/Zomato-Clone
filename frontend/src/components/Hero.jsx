0b8cff2393fc430d9ae9351560e61c780bedf879
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-19 04:05:50 +0530

    chore: Refactor Hero component to improve code readability and formatting

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,130 +3,130 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
 
-    const cities = [
-        'Agra',
-        'Ahmedabad',
-        'Ajmer',
-        'Alappuzha',
-        'Allahabad',
-        'Amravati',
-        'Amritsar',
-        'Aurangabad',
-        'Bengaluru',
-        'Bhopal',
-        'Bhubaneshwar',
-        'Chandigarh',
-        'Chennai',
-        'Coimbatore',
-        'Cuttack',
-        'Darjeeling',
-        'Dehradun',
-        'Delhi NCR',
-        'Dharamsala',
-        'Gangtok',
-        'Goa',
-        'Gorakhpur',
-        'Guntur',
-        'Guwahati',
-        'Gwalior',
-        'Haridwar',
-        'Hyderabad',
-        'Indore',
-        'Jabalpur',
-        'Jaipur',
-        'Jalandhar',
-        'Jammu',
-        'Jamnagar',
-        'Jamshedpur',
-        'Jhansi',
-        'Jodhpur',
-        'Junagadh',
-        'Kanpur',
-        'Khajuraho',
-        'Khamgaon',
-        'Kharagpur',
-        'Kochi',
-        'Kolhapur',
-        'Kolkata',
-        'Kota',
-        'Lucknow',
-        'Ludiana',
-        'Madurai',
-        'Manali',
-        'Mangalore',
-        'Manipal',
-        'Meerut',
-        'Mumbai',
-        'Mussoorie',
-        'Mysore',
-        'Nagpur',
-        'Nanital',
-        'Nashik',
-        'Neemrana',
-        'Ooty',
-        'Palakkad',
-        'Patiala',
-        'Patna',
-        'Puducherry',
-        'Pune',
-        'Pushkar',
-        'Raipur',
-        'Rajkot',
-        'Ranchi',
-        'Rishikesh',
-        'Salem',
-        'Shimla',
-        'Siliguri',
-        'Srinagar',
-        'Surat',
-        'Thrissur',
-        'Tirupati',
-        'Trichy',
-        'Trivandrum',
-        'Udaipur',
-        'Vadodara',
-        'Varanasi',
-        'Vellore',
-        'Vijayawada',
-        'Visakhapatnam'
-    ]
+  const cities = [
+    'Agra',
+    'Ahmedabad',
+    'Ajmer',
+    'Alappuzha',
+    'Allahabad',
+    'Amravati',
+    'Amritsar',
+    'Aurangabad',
+    'Bengaluru',
+    'Bhopal',
+    'Bhubaneshwar',
+    'Chandigarh',
+    'Chennai',
+    'Coimbatore',
+    'Cuttack',
+    'Darjeeling',
+    'Dehradun',
+    'Delhi NCR',
+    'Dharamsala',
+    'Gangtok',
+    'Goa',
+    'Gorakhpur',
+    'Guntur',
+    'Guwahati',
+    'Gwalior',
+    'Haridwar',
+    'Hyderabad',
+    'Indore',
+    'Jabalpur',
+    'Jaipur',
+    'Jalandhar',
+    'Jammu',
+    'Jamnagar',
+    'Jamshedpur',
+    'Jhansi',
+    'Jodhpur',
+    'Junagadh',
+    'Kanpur',
+    'Khajuraho',
+    'Khamgaon',
+    'Kharagpur',
+    'Kochi',
+    'Kolhapur',
+    'Kolkata',
+    'Kota',
+    'Lucknow',
+    'Ludiana',
+    'Madurai',
+    'Manali',
+    'Mangalore',
+    'Manipal',
+    'Meerut',
+    'Mumbai',
+    'Mussoorie',
+    'Mysore',
+    'Nagpur',
+    'Nanital',
+    'Nashik',
+    'Neemrana',
+    'Ooty',
+    'Palakkad',
+    'Patiala',
+    'Patna',
+    'Puducherry',
+    'Pune',
+    'Pushkar',
+    'Raipur',
+    'Rajkot',
+    'Ranchi',
+    'Rishikesh',
+    'Salem',
+    'Shimla',
+    'Siliguri',
+    'Srinagar',
+    'Surat',
+    'Thrissur',
+    'Tirupati',
+    'Trichy',
+    'Trivandrum',
+    'Udaipur',
+    'Vadodara',
+    'Varanasi',
+    'Vellore',
+    'Vijayawada',
+    'Visakhapatnam'
+  ]
 
   return (
     <>
-        <div className=' container max-w-6xl mx-auto p-5'>
-            <p className="justify-center text-4xl font-normal tracking-wide my-5 flex">Popular locations in <span className='flex uppercase font-semibold'><img className='mx-4' width={50} src='/india-flag-icon.svg'/> India</span> </p>
-            <p className='text-2xl max-w-4xl mx-auto text-center text-gray-500 font-light py-5 '>From swanky upscale restaurants to the cosiest hidden gems serving the most incredible food, Zomato covers it all. Explore menus, and millions of restaurant photos and reviews from users just like you, to find your next great meal.</p>
-            <div className=' grid grid-cols-3 gap-6 gap-y-8'>
-                {
-                    cities.map((city)=>{
-                        return (
-                            <div key={city}>
-                <Link to={'/'+city.toLowerCase()} className="flex items-center justify-between p-5 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl">
-  <span className="w-full text-xl font-normal text-black">{city} Restaurants</span>
-  <svg
-    className="w-4 h-4 ms-2 rtl:rotate-180"
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 14 10"
-  >
-    <path
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M1 5h12m0 0L9 1m4 4L9 9"
-    />
-  </svg>
-</Link>
+      <div className=' container max-w-6xl mx-auto p-5'>
+        <p className="justify-center text-4xl font-normal tracking-wide my-5 flex">Popular locations in <span className='flex uppercase font-semibold'><img className='mx-4' width={50} src='/india-flag-icon.svg' /> India</span> </p>
+        <p className='text-2xl max-w-4xl mx-auto text-center text-gray-500 font-light py-5 '>From swanky upscale restaurants to the cosiest hidden gems serving the most incredible food, Zomato covers it all. Explore menus, and millions of restaurant photos and reviews from users just like you, to find your next great meal.</p>
+        <div className=' grid grid-cols-3 gap-6 gap-y-8'>
+          {
+            cities.map((city) => {
+              return (
+                <div key={city}>
+                  <Link to={'/' + city.toLowerCase()} className="flex border items-center justify-between p-5 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl">
+                    <span className="w-full text-xl font-normal text-black">{city} Restaurants</span>
+                    <svg
+                      className="w-4 h-4 ms-2 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </Link>
 
                 </div>
-                        )
-                    })
-                }
+              )
+            })
+          }
 
 
-                {/* <div>
+          {/* <div>
                 <a href="#" className="flex items-center justify-between p-5 text-base font-medium text-black rounded-lg bg-gray-50 shadow hover:shadow-xl">
   <span className="w-full text-xl font-normal text-black">Agragggggggkkkk Restaurants</span>
   <svg
@@ -148,8 +148,8 @@ const Hero = () => {
 
                 </div> */}
 
-            </div>
         </div>
+      </div>
     </>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import HomeCard from '../components/HomeCard'
 import styled from 'styled-components'
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { Link, useParams } from 'react-router-dom';
 
 const Section = styled.section`
   display:flex;
@@ -44,7 +45,90 @@ const HomePage = () => {
     }
   ]
 
+  const LucknowCities = [
+    {
+      city: 'Gomti Nagar',
+      place: '1689 places'
+    },
+    {
+      city: 'Hazratganj',
+      place: '1689 places'
+    },
+    {
+      city: 'Aliganj',
+      place: '1689 places'
+    },
+    {
+      city: 'Indira Nagar',
+      place: '1689 places'
+    },
+    {
+      city: 'Alambagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Charbagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Aminabad',
+      place: '1689 places'
+    },
+    {
+      city: 'Jankipuram',
+      place: '1689 places'
+    },
+    {
+      city: 'Gomti Nagar',
+      place: '1689 places'
+    },
+    {
+      city: 'Hazratganj',
+      place: '1689 places'
+    },
+    {
+      city: 'Aliganj',
+      place: '1689 places'
+    },
+    {
+      city: 'Indira Nagar',
+      place: '1689 places'
+    },
+    {
+      city: 'Alambagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Charbagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Alambagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Charbagh',
+      place: '1689 places'
+    },
+    {
+      city: 'Jankipuram',
+      place: '1689 places'
+    }
+  ]
+
+  const selectedCity = useParams().city;
+  console.log(selectedCity);
   const [activeRadio, setActiveRadio] = useState('email');
+  const [hiddenList, setHiddenList] = useState('hidden');
+
+  const handleList = ()=>{
+    if(hiddenList === 'hidden'){
+      setHiddenList('block');
+    }
+    else{
+      setHiddenList('hidden');
+    }
+  }
 
   return (
     <>
@@ -82,6 +166,83 @@ const HomePage = () => {
         </div>
       </Section>
 
+      <section className='py-10'>
+        <div className='max-w-screen-xl mx-auto p-10'>
+          <p className='text-4xl mb-10'>Popular localites in and around <span className='font-semibold capitalize'>{selectedCity}</span>  </p>
+          <div>
+            <div className=' grid grid-cols-3 gap-6 gap-y-8'>
+              {
+                LucknowCities.map((city,index) => {
+                  if(index!==LucknowCities.length-1 && index<8){
+                    return (
+                      <div key={index}>
+                      <Link to={'/' + city} className="flex border items-center justify-between p-3 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl">
+                        {/* <span className="w-full text-xl font-normal text-black">{city} Restaurants</span> */}
+                        <div>
+                          <p className='text-xl font-normal'>{city.city} </p>
+                          <p className='font-light'>{city.place} </p>
+                        </div>
+                        <svg
+                          className="text-gray-500 w-4 h-4 ms-2 rtl:rotate-180"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </Link>
+                    </div>
+                    )
+                  }
+                  else if(index===LucknowCities.length-1){
+                    return (
+                      <div onClick={handleList} className={ hiddenList === 'hidden'? `flex border justify-center items-center p-3 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl` :` border justify-center items-center p-3 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl hidden`} >
+                        <p className='text-xl font-medium'> {hiddenList==='hidden'?'see more':'see less'} </p>
+                      </div>
+                    )
+                  }
+                  else{
+                    return (
+                      <div key={index} className={hiddenList}>
+                      <Link to={'/' + city} className="flex border items-center justify-between p-3 text-base font-medium text-black rounded-lg bg-gray-50 shadow-md hover:shadow-xl">
+                        {/* <span className="w-full text-xl font-normal text-black">{city} Restaurants</span> */}
+                        <div>
+                          <p className='text-xl font-normal'>{city.city} </p>
+                          <p className='font-light'>{city.place} </p>
+                        </div>
+                        <svg
+                          className="text-gray-500 w-4 h-4 ms-2 rtl:rotate-180"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </Link>
+                    </div>
+                    )
+                  }
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className='my-10 bg-[#FFFBF7] '>
         <div className='py-10 max-w-screen-md w-30 mx-auto flex sm:flex-row flex-col items-center px-5 gap-4 sm:items-end '>
           <img alt='image' className=' w-[250px] h-[250px]' src='https://b.zmtcdn.com/data/o2_assets/f773629053b24263e69f601925790f301680693809.png' />
@@ -90,27 +251,27 @@ const HomePage = () => {
             <p className='text-lg font-light'>We will send you a link, open it on your phone to download the app</p>
             <div className=' flex items-center justify-start gap-12'>
               <div className='flex items-center gap-2'>
-                <input type='radio' value='email' onChange={()=>setActiveRadio('email')} id='Email' className='text-red-500' name='radio-input'/>
+                <input type='radio' value='email' onChange={() => setActiveRadio('email')} id='Email' className='text-red-500' name='radio-input' />
                 <label>Email</label>
               </div>
               <div className='flex items-center gap-2'>
-                <input type='radio' value='phone' onChange={()=>setActiveRadio('phone')} id='phone' className='text-red-500' name='radio-input' />
+                <input type='radio' value='phone' onChange={() => setActiveRadio('phone')} id='phone' className='text-red-500' name='radio-input' />
                 <label>Phone</label>
               </div>
             </div>
             <div className='flex gap-2 justify-evenly'>
               {/* <input type='email' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400'  placeholder='Email' /> */}
               {/* <input type='email' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400'  placeholder='Phone' /> */}
-             
+
               {
-                activeRadio === 'email' ? <input type='email' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400'  placeholder='Email' /> : <input type='number' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400'  placeholder='Phone' />
+                activeRadio === 'email' ? <input type='email' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400' placeholder='Email' /> : <input type='number' className='flex-1 rounded outline-none border border-gray-300 p-2.5 placeholder:text-gray-400' placeholder='Phone' />
               }
               <button className='bg-[#EF4F5F] rounded-md hover:bg-red-700 text-white px-6'>Share App Link</button>
             </div>
             <p className='text-[#9C9C9C]'>Download app from</p>
             <div className='flex gap-4'>
               <img alt='image' className='w-[120px] h-[40px]' src='https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png' />
-              <img alt='image' className='w-[120px] h-[40px]' src='https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png' />  
+              <img alt='image' className='w-[120px] h-[40px]' src='https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png' />
             </div>
           </div>
         </div>
