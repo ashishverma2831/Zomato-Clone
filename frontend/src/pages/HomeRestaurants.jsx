34a7b8b14dd834a6f075cab391bf85b90be3c6b4
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-21 01:05:06 +0530

    Update HomeRestaurants component to include tab functionality for dining, delivery, and nightlife

--- frontend/src/pages/HomeRestaurants.jsx
+++ frontend/src/pages/HomeRestaurants.jsx
@@ -4,12 +4,17 @@ const HomeRestaurants = () => {
 
     const [sidebar, setSidebar] = useState(false);
 
+    const [activeTab, setActiveTab] = useState("dining");
+    const handleTabs = (tab) => {
+        setActiveTab(tab);
+    }
+
     return (
         <>
             <header>
                 {
                     sidebar ? (<div className='h-[100vh] p-5 absolute md:z-[-1] w-64 bg-gray-300'>
-                        <p className='text-2xl font-semibold mb-10 cursor-pointer' onClick={()=>{setSidebar(!sidebar)}}>X</p>
+                        <p className='text-2xl font-semibold mb-10 cursor-pointer' onClick={() => { setSidebar(!sidebar) }}>X</p>
                         <div className='flex flex-col w-[200px] gap-4'>
                             <p className='text-lg hover:bg-gray-50 p-2 rounded'>Log in</p>
                             <p className='text-lg hover:bg-gray-50 p-2 rounded'>Sign up</p>
@@ -18,7 +23,7 @@ const HomeRestaurants = () => {
                 }
                 <div className='max-w-screen-lg gap-4 mx-auto justify-between items-center flex '>
                     <div className='flex flex-1 gap-4 justify-between items-center p-5 md:py-0'>
-                        <div className='block md:hidden cursor-pointer' onClick={()=>{setSidebar(!sidebar)}}>
+                        <div className='block md:hidden cursor-pointer' onClick={() => { setSidebar(!sidebar) }}>
                             <div className='flex flex-col gap-1'>
                                 <p className='w-7 h-1 rounded bg-slate-800'>  </p>
                                 <p className='w-7 h-1 rounded bg-slate-800'>  </p>
@@ -36,6 +41,17 @@ const HomeRestaurants = () => {
                     </div>
                 </div>
             </header>
+
+            <main className='bg-red-400'>
+                <div className='max-w-screen-lg flex mx-auto px-5 '>
+                    <div onClick={()=>{handleTabs("dining")}} className={activeTab === "dining" ?'p-4 text-xl text-red-500 font-semibold flex items-center gap-2 border-b-red-500 border-b-4' :'p-4 text-xl text-red-500 font-semibold flex items-center gap-2' }> <div className='p-4 rounded-full bg-[#E5F3F3]'><img className='h-8 w-8' src={activeTab==="dining"?'https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png':'https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png'} /></div>  Dining Out</div>
+                    <div onClick={()=>{handleTabs("delivery")}} className={activeTab === "delivery" ?'p-4 text-xl text-red-500 font-semibold flex items-center gap-2 border-b-red-500 border-b-4' :'p-4 text-xl text-red-500 font-semibold flex items-center gap-2' }> <div className='p-4 rounded-full bg-[#E5F3F3]'><img className='h-8 w-8' src={activeTab==="delivery"?'https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png':'https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png'} /></div>  Delivery</div>
+                    <div onClick={()=>{handleTabs("nightlife")}} className={activeTab === "nightlife" ?'p-4 text-xl text-red-500 font-semibold flex items-center gap-2 border-b-red-500 border-b-4' :'p-4 text-xl text-red-500 font-semibold flex items-center gap-2' }> <div className='p-4 rounded-full bg-[#E5F3F3]'><img className='h-8 w-8' src={activeTab==="nightlife"?'https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png':'https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png'} /></div>  Nightlife</div>
+                </div>
+                <div className='bg-blue-500'>
+                    <p>{activeTab} </p>
+                </div>
+            </main>
         </>
     )
 }
