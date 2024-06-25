b69b077ec68847e29492d76d0ed1f0011c5b14db
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-26 01:51:24 +0530

    Update DiningOut component to include additional RestaurantCard

--- frontend/src/components/DiningOut.jsx
+++ frontend/src/components/DiningOut.jsx
@@ -16,24 +16,24 @@ const DiningOut = () => {
                 <div className='max-w-screen-xl mx-auto md:p-8'>
                     <div className='w-full'>
                         <p className='text-4xl text-left px-2 pt-2'>Collections</p>
-                        <div className='flex bg-red-400 justify-between p-2 gap-2'>
+                        <div className='flex justify-between p-2 gap-2'>
                             <p className='text-lg md:text-xl text-gray-500 '>Explore curated lists of top restaurants, cafes, pubs, and bars in Lucknow, based on trends</p>
                             <p className='flex justify-between '><button className='text-red-500 text-lg md:text-xl'>All collections in Lucknow <i className="text-sm mx-2 fa-solid fa-play"></i></button></p>
                         </div>
                         <div className='p-3 flex gap-4 justify-evenly flex-wrap'>
-                            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+                            <div className='cursor-pointer flex-1 h-[360px] min-w-[150px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
                                 <p>Best insta-worthy places</p>
                                 <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
                             </div>
-                            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+                            <div className='cursor-pointer flex-1 h-[360px] min-w-[150px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
                                 <p>Best insta-worthy places</p>
                                 <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
                             </div>
-                            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+                            <div className='cursor-pointer flex-1 h-[360px] min-w-[150px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
                                 <p>Best insta-worthy places</p>
                                 <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
                             </div>
-                            <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
+                            <div className='cursor-pointer flex-1 h-[360px] min-w-[150px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
                                 <p>Best insta-worthy places</p>
                                 <p>9 places <i className="text-sm mx-2 fa-solid fa-play"></i></p>
                             </div>
@@ -58,6 +58,7 @@ const DiningOut = () => {
                 <div className='py-5 px-10 max-w-screen-xl mx-auto '>
                     <p className='px-2 text-4xl font-normal'>T. Nagar Restaurants, Chennai</p>
                     <div className='flex flex-wrap py-5 gap-2 md:gap-6'>
+                        <RestaurantCard />
                         <RestaurantCard />
                         <RestaurantCard />
                         <RestaurantCard />
