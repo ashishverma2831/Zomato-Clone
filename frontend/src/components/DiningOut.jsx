ee597bfcbbcbfe924f3a50aca28895c0d7ac6c05
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-26 01:45:15 +0530

    Update dimensions and styling of AdvertisementCard component

--- frontend/src/components/DiningOut.jsx
+++ frontend/src/components/DiningOut.jsx
@@ -13,12 +13,12 @@ const DiningOut = () => {
     return (
         <>
             <section className='bg-gray-100'>
-                <div className='max-w-screen-xl mx-auto p-8'>
+                <div className='max-w-screen-xl mx-auto md:p-8'>
                     <div className='w-full'>
-                        <p className='text-4xl text-left px-3'>Collections</p>
-                        <div className='flex justify-between p-3'>
-                            <p className='text-xl text-gray-500'>Explore curated lists of top restaurants, cafes, pubs, and bars in Lucknow, based on trends</p>
-                            <p className='flex justify-between'><button className='text-red-500 text-xl'>All collections in Lucknow <i className="text-sm mx-2 fa-solid fa-play"></i></button></p>
+                        <p className='text-4xl text-left px-2 pt-2'>Collections</p>
+                        <div className='flex bg-red-400 justify-between p-2 gap-2'>
+                            <p className='text-lg md:text-xl text-gray-500 '>Explore curated lists of top restaurants, cafes, pubs, and bars in Lucknow, based on trends</p>
+                            <p className='flex justify-between '><button className='text-red-500 text-lg md:text-xl'>All collections in Lucknow <i className="text-sm mx-2 fa-solid fa-play"></i></button></p>
                         </div>
                         <div className='p-3 flex gap-4 justify-evenly flex-wrap'>
                             <div className='flex-1 h-[360px] min-w-[240px] flex flex-col text-xl rounded-lg items-start text-white p-5 justify-end bg-center bg-cover bg-[url(https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*)]'>
