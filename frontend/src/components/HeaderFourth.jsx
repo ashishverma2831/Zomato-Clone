29b53a92e580e0cc1d385c92eb19c02b647c9b4c
ashishverma2831 <ashishverma510207@gmail.com>
2024-06-27 07:58:27 +0530

    feat: Add lazy loading for images in Nightlife section

--- frontend/src/components/HeaderFourth.jsx
+++ frontend/src/components/HeaderFourth.jsx
@@ -12,13 +12,19 @@ const HeaderFourth = () => {
         <>
             <header>
                 {
-                    sidebar ? (<div className='h-[100vh] p-5 absolute md:z-[-1] w-64 bg-gray-300'>
-                        <p className='text-2xl font-semibold mb-10 cursor-pointer' onClick={() => { setSidebar(!sidebar) }}>X</p>
+                    sidebar ? (<div className='fixed top-0 left-0 z-40 h-screen p-5 md:z-[-1] block md:hidden w-full bg-white'>
+                        <div className='flex justify-between items-center mb-10'>
+                            <p className='text-4xl font-normal cursor-pointer' onClick={() => { setSidebar(!sidebar) }}>
+                                <span className=''>
+                                    <p className='w-6 h-[3px] translate-y-1/2 rotate-45 rounded bg-slate-800'>  </p>
+                                    <p className='w-6 h-[3px] -translate-y-1/2 rounded -rotate-45 bg-slate-800'>  </p>
+                                </span>
+                            </p>
+                            <p className='text-4xl bold italic font-extrabold'>Zomato</p>
+                        </div>
                         <div className='flex flex-col w-[200px] gap-4'>
                             <button
-                                data-modal-target="authentication-modal"
-                                data-modal-toggle="authentication-modal"
-                                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 text-white"
+                                className="w-full text-left p-2 text-gray-500 hover:text-gray-800 text-lg"
                                 type="button"
                                 onClick={() => {
                                     setLogInModal(true)
@@ -26,14 +32,12 @@ const HeaderFourth = () => {
                             >
                                 Log in
                             </button>
-                            {LogInModal ? <LogIn modalId="authentication-modal" closeFunc={() => { setLogInModal(false) }} func={() => {
+                            {LogInModal ? <LogIn closeFunc={() => { setLogInModal(false) }} func={() => {
                                 setLogInModal(false)
                                 setSignUpModal(true)
                             }} /> : <></>}
                             <button
-                                data-modal-target="authentication-modal-4"
-                                data-modal-toggle="authentication-modal-4"
-                                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 text-white"
+                                className="w-full text-left p-2 text-gray-500 hover:text-gray-800 text-lg"
                                 type="button"
                                 onClick={() => {
                                     setSignUpModal(true)
@@ -41,7 +45,7 @@ const HeaderFourth = () => {
                             >
                                 Sign up
                             </button>
-                            {SignUpModal ? <SignUp modalId="authentication-modal-4" closeFunc={() => { setSignUpModal(false) }} func={() => {
+                            {SignUpModal ? <SignUp closeFunc={() => { setSignUpModal(false) }} func={() => {
                                 setSignUpModal(false)
                                 setLogInModal(true)
                             }} /> : <></>}
@@ -52,22 +56,20 @@ const HeaderFourth = () => {
                     <div className='flex flex-1 gap-4 justify-between items-center p-5 md:py-0'>
                         <div className='block md:hidden cursor-pointer' onClick={() => { setSidebar(!sidebar) }}>
                             <div className='flex flex-col gap-1'>
-                                <p className='w-7 h-1 rounded bg-slate-800'>  </p>
-                                <p className='w-7 h-1 rounded bg-slate-800'>  </p>
-                                <p className='w-7 h-1 rounded bg-slate-800'>  </p>
+                                <p className='w-6 h-[3px] rounded bg-slate-800'>  </p>
+                                <p className='w-6 h-[3px] rounded bg-slate-800'>  </p>
+                                <p className='w-6 h-[3px] rounded bg-slate-800'>  </p>
                             </div>
                         </div>
                         <p className='text-4xl bold italic font-extrabold'>Zomato</p>
                         <input type='text' autoFocus={true} placeholder='Search for restaurant, cuisine or a dish' className='w-full hidden md:block shadow-lg py-3 px-4 text-xl my-4 font-normal placeholder-gray-500 border border-gray-200 focus:outline-none outline-none rounded-lg' />
                     </div>
                     <div className='hidden md:block'>
-                        <div className='flex w-[200px] justify-between items-center px-5 gap-2'>
+                        <div className='flex w-[200px] justify-between items-center px-5 gap-4'>
                             {/* <p className='text-lg text-gray-500 hover:text-gray-700'>Log in</p>
                             <p className='text-lg text-gray-500 hover:text-gray-700'>Sign up</p> */}
                             <button
-                                data-modal-target="authentication-modal"
-                                data-modal-toggle="authentication-modal"
-                                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 text-white"
+                                className="hover:text-gray-800 p-2 text-lg text-gray-500"
                                 type="button"
                                 onClick={() => {
                                     setLogInModal(true)
@@ -75,14 +77,12 @@ const HeaderFourth = () => {
                             >
                                 Log in
                             </button>
-                            {LogInModal ? <LogIn modalId="authentication-modal" closeFunc={() => { setLogInModal(false) }} func={() => {
+                            {LogInModal ? <LogIn closeFunc={() => { setLogInModal(false) }} func={() => {
                                 setLogInModal(false)
                                 setSignUpModal(true)
                             }} /> : <></>}
                             <button
-                                data-modal-target="authentication-modal-4"
-                                data-modal-toggle="authentication-modal-4"
-                                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 text-white"
+                                className="hover:text-gray-800 p-2 text-lg text-gray-500"
                                 type="button"
                                 onClick={() => {
                                     setSignUpModal(true)
@@ -90,7 +90,7 @@ const HeaderFourth = () => {
                             >
                                 Sign up
                             </button>
-                            {SignUpModal ? <SignUp modalId="authentication-modal-4" closeFunc={() => { setSignUpModal(false) }} func={() => {
+                            {SignUpModal ? <SignUp closeFunc={() => { setSignUpModal(false) }} func={() => {
                                 setSignUpModal(false)
                                 setLogInModal(true)
                             }} /> : <></>}
