e29950eb61c4241d8c632e09da0e86fe0b64d40e
ashishverma2831 <ashishverma510207@gmail.com>
2024-07-02 15:05:59 +0530

    refactor: Update Collections page layout for better responsiveness

--- frontend/src/components/RestaurantCard.jsx
+++ frontend/src/components/RestaurantCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RestaurantCard = () => {
   return (
     <>
-        <div className='w-[330px] cursor-pointer flex-1 bg-transparent hover:border hover:bg-white hover:shadow-lg p-2 hover:rounded-xl'>
+        <div className='w-[330px] cursor-pointer flex-1 hover:flex-3 bg-transparent hover:border hover:bg-white hover:shadow-lg p-2 hover:rounded-xl'>
             <img className='w-full rounded-xl' src='https://b.zmtcdn.com/data/pictures/3/18972533/ecfa428872197028747732c7e0f4ebd0_featured_v2.jpg' alt='restaurant' />
             <div className='flex flex-col py-2'>
                 <div className=' flex justify-between items-center '>
