432a20b37fca038aeefcb1d641e051b9cfa96ebe
ashishverma2831 <ashishverma510207@gmail.com>
2024-08-06 18:19:15 +0530

    refactor: Remove commented code and unused state variables in Photos component

--- frontend/src/components/Photos.jsx
+++ frontend/src/components/Photos.jsx
@@ -378,19 +378,33 @@ const Photos = () => {
   const [imagelist, setImagelist] = useState(imagesData);
   console.log(imagelist);
   const imagePerPage = 12;
+
+  // const [filter, setFilter] = useState('all');
+  // console.log(filter);
   
+  // const handleFilter = (filter) => {
+  //   if(filter === 'food') {
+  //     const newImagelist = imagelist.filter((image) => image.category === 'food');
+  //     setImagelist(newImagelist);
+  //   }else if(filter === 'ambience') {
+  //     const newImagelist = imagelist.filter((image) => image.category === 'ambience');
+  //     setImagelist(newImagelist);
+  //   }else {
+  //     setImagelist(imagesData);
+  //   }
+  // }
 
   return (
     <>
       <section className='border-b-2'>
         <p className='text-2xl font-semibold'>Garden Grille & Bar - Hilton Garden Inn Photos</p>
-        <div>
+        {/* <div>
           <div className='flex mt-6 gap-4'>
-            <button className='text-gray-500 rounded-md border py-2 px-4'>All (247)</button>
-            <button className='text-gray-500 rounded-md border py-2 px-4'>Food (184)</button>
-            <button className='text-gray-500 rounded-md border py-2 px-4'>Ambience (29)</button>
+            <button onClick={()=>{ handleFilter('all') }} className='text-gray-500 rounded-md border py-2 px-4'>All </button>
+            <button onClick={()=>{ handleFilter('food') }} className='text-gray-500 rounded-md border py-2 px-4'>Food</button>
+            <button onClick={()=>{ handleFilter('ambience') }} className='text-gray-500 rounded-md border py-2 px-4'>Ambience </button>
           </div>
-        </div>
+        </div> */}
         <Pagination imagelist={imagelist} imagePerPage={imagePerPage} />
       </section>
     </>
